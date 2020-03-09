efd54890c90cb612a3bd2682a26331f0ce9980d3
Owen Bezick <owbezick@davidson.edu>
2020-03-09 12:19:50 -0400

    Graphs

--- R/Template Dashboard/app.R
+++ R/Template Dashboard/app.R
@@ -18,9 +18,10 @@ ui <- dashboardPagePlus(
     # Sidebar ----
     , dashboardSidebar( # Contains a sidebarMenu with menuItems and subMenuItems
         sidebarMenu(
-            menuItem(tabName = "tabOne", text = "HTML Output", icon = icon("one")) # menuItem
-            , menuItem(tabName = "tabTwo", text = "uiOutput", icon = icon("two"))
-            , menuItem(tabName = "tabThree", text = "Action Button & Modal Viewer"))
+            menuItem(tabName = "tabOne", text = "Text Output", icon = icon("one")) # menuItem
+            , menuItem(tabName = "tabTwo", text = "DT Output", icon = icon("two"))
+            , menuItem(tabName = "tabThree", text = "eCharts4R Output", icon = icon("two"))
+            , menuItem(tabName = "tabFour", text = "Action Button & Modal Viewer"))
     )
     # Body ----
     , dashboardBody( # Contains tabItems
@@ -32,12 +33,19 @@ ui <- dashboardPagePlus(
             )
             , tabItem(
                 tabName = "tabTwo"
-                , box(width = 12, title = "Cars Datatable", status = "primary" # A box is a UI element that encloses uiOutputs, such as datatables
-                      , DTOutput("carsDT") # DTOutput isused for DT tables, instead of uiOutput.
+                , box(width = 12, title = "Sample Datatable", status = "primary" # A box is a UI element that encloses uiOutputs, such as datatables
+                      , DTOutput("carsDT") # DTOutput is used for DT tables, instead of uiOutput.
                 )
             )
             , tabItem(
                 tabName = "tabThree"
+                , box(width = 12, title = "Sample Graphs", status = "danger", collapsible = TRUE
+                      , echarts4rOutput("sample_line") # echarts4rOutput is used for echarts4r charts, instead of uiOutput or DTOutput
+                      , echarts4rOutput("sample_bar")
+                )
+            )
+            , tabItem(
+                tabName = "tabFour"
                 # Action buttons can be implemented directly in the UI or in the server
                 , actionBttn(inputId = 'modal' # the inputId will be used in the server in an observeEvent call
                              , label = "Click for Modal Viewer"
@@ -67,6 +75,30 @@ server <- function(input, output) {
         datatable(carsData)
     })
     
+    # Graphs ----
+    # Fake Data
+    df <- data.frame(
+        x = seq(50),
+        y = rnorm(50, 10, 3),
+        z = rnorm(50, 11, 2),
+        w = rnorm(50, 9, 2)
+    )
+    # Line Chart 
+    output$sample_line <- renderEcharts4r({
+        df %>% 
+            e_charts(x) %>% 
+            e_line(z) %>% 
+            e_area(w) %>% 
+            e_title("Line and area charts")
+    })
+    
+    output$sample_bar <- renderEcharts4r({
+        df %>% 
+            e_charts(x) %>% 
+            e_bar(y, name = "Serie 1") %>% 
+            e_step(z, name = "Serie 2") %>% 
+            e_title("Bar and step charts")
+    })
 }
 
 # Run the application 
