947db7ad5d891dac0ff3f4f97cf960088d10e6fc
Owen Bezick <owbezick@davidson.edu>
2020-04-10 11:38:29 -0400

    review grades / edit tabs

--- R/Template Dashboard/app.R
+++ R/Template Dashboard/app.R
@@ -13,28 +13,54 @@ source("libraries.R", local = TRUE)
 source("dataIntake.R", local = TRUE)
 
 # UI ----
-ui <- dashboardPagePlus(
-    dashboardHeaderPlus(title = "Professor View" # Creates dashboardHeaderPlus title (can inject javascript here to add pictures, fonts, etc.)
+ui <- dashboardPage(
+    dashboardHeader(title = "Professor View" # Creates dashboardHeaderPlus title (can inject javascript here to add pictures, fonts, etc.)
     )
     # Sidebar ----
     , dashboardSidebar( # Contains a sidebarMenu with menuItems and subMenuItems
         sidebarMenu(
-            menuItem(tabName = "reviewGrades", text = "View Review Grades", icon = icon("one")) # menuItem
-            , menuItem(tabName = "homeworkGrades", text = "View Homework Grades", icon = icon("A"))
+            menuItem(tabName = "home", text = "Home", icon = icon("home"))
+            , menuItem(tabName ="viewGrades", text = "View Grades", icon = icon("chalkboard")
+                       , menuSubItem(tabName = "reviewGrades", text = "View Review Grades")
+                       , menuSubItem(tabName = "homeworkGrades", text = "View Homework Grades")
+            )
+            , menuItem(tabName ="editGrades", text = "Edit Grades", icon = icon("chalkboard-teacher")
+                       , menuSubItem(tabName = "editReviewGrades", text = "Edit Review Grades")
+                       , menuSubItem(tabName = "editHomeworkGrades", text = "Edit Homework Grades")
+            )
         )
     )
     # Body ----
     , dashboardBody( # Contains tabItems
         tabItems(
+            # Home UI ----
             tabItem(
-                tabName = "reviewGrades"
-                , HTML("Sample <b> HTML </b> output") # This is HTML output directly in the UI
-                , uiOutput("text") # This is a uiOutput from the server
+                tabName = "home"
             )
+            # Review UI ----
+            , tabItem(
+                tabName = "reviewGrades"
+                ,fluidRow(
+                    box(width = 12, title = "Filter:", status = "primary" 
+                        ,column(width = 6
+                                ,uiOutput("reviewStudentPicker")
+                        )
+                        , column(width = 6
+                                 ,uiOutput("reviewPicker")
+                        )
+                    )
+                )
+                , fluidRow(
+                    box(width = 12, status = "primary"
+                        , DTOutput("totalReviewGrades")
+                    )
+                )
+            )
+            # Homework UI ----
             , tabItem(
                 tabName = "homeworkGrades"
                 , fluidRow(
-                    box(width = 12, title = "Filter:", status = "primary" # A box is a UI element that encloses uiOutputs, such as datatables
+                    box(width = 12, title = "Filter:", status = "primary" 
                         ,column(width = 6
                                 ,uiOutput("hwStudentPicker")
                         )
@@ -42,11 +68,20 @@ ui <- dashboardPagePlus(
                                  ,uiOutput("hwPicker")
                         )
                     )
-                    , box(width = 12, status = "primary"
-                          , DTOutput("homeworkGradeTable")
+                )
+                , fluidRow(
+                    box(width = 12, status = "primary"
+                        , DTOutput("homeworkGradeTable")
                     )
                 )
             )
+            , tabItem(
+                tabName = "editReviewGrades"
+            )
+            , tabItem(
+                tabName = "editHomeworkGrades"
+            )
+            
         )
     )
 )
@@ -54,24 +89,53 @@ ui <- dashboardPagePlus(
 
 # Define server logic 
 server <- function(input, output) {
-    # renderText output ----
-    output$text <- renderText("Sample renderText output.") # Output to be used in the UI
+    # Review Server ---- 
     
-    # Observe Event w/ Modal Viewer ----
-    observeEvent(input$modal, { #When input$modal is no longer NULL (after the button is clicked) the code in the observe event will run
-        showModal(
-            modalDialog(title = "This is a modal dialog!")
-        )
+    # List of students
+    ls_studentsR <- reactive({
+        df <- getReviewGrades()
+        df %>% distinct(student_id) %>% pull()
+    })
+    #List from homework
+    ls_reviews<- reactive({
+        df <- getReviewGrades()
+        df %>% distinct(review_id) %>% pull()
+    })
+    # Student Picker
+    output$reviewStudentPicker <- renderUI({
+        pickerInput("reviewStudentPicker"
+                    ,"Student by ID"
+                    , choices = ls_studentsR()
+                    , selected = ls_studentsR()
+                    , multiple = TRUE)
+    })
+    # Homework Picker
+    output$reviewPicker <- renderUI({
+        pickerInput("reviewPicker"
+                    ,"Review by ID"
+                    , choices = ls_reviews()
+                    , selected = ls_reviews()
+                    , multiple = TRUE)
+    })
+    
+    # DT output
+    output$totalReviewGrades <- renderDT({
+        req(input$reviewStudentPicker, input$reviewPicker)
+        df <- getReviewGrades()
+        df <- df %>%
+            filter(review_id %in% input$reviewPicker, student_id %in% input$reviewStudentPicker) %>%
+            select(review_id, First = first_name, Last = last_name, Topic = topic_id, Grade = grade)
+        datatable(df, rownames = FALSE)
     })
     
     # Homeworks Server -----
     # List of students
-    ls_students <- reactive({
+    ls_studentsHW <- reactive({
         df <- getHomeworkGrades()
         df %>% distinct(student_id) %>% pull()
     })
     #List from homework
-    ls_homeworks <- reactive({
+    ls_homeworksHW <- reactive({
         df <- getHomeworkGrades()
         df %>% distinct(homework_id) %>% pull()
     })
@@ -79,16 +143,16 @@ server <- function(input, output) {
     output$hwStudentPicker <- renderUI({
         pickerInput("hwStudentPicker"
                     ,"Student by ID"
-                    , choices = ls_students()
-                    , selected = ls_students()
+                    , choices = ls_studentsHW()
+                    , selected = ls_studentsHW()
                     , multiple = TRUE)
     })
     # Homework Picker
     output$hwPicker <- renderUI({
         pickerInput("hwPicker"
                     ,"Homework by ID"
-                    , choices = ls_homeworks()
-                    , selected = ls_homeworks()
+                    , choices = ls_homeworksHW()
+                    , selected = ls_homeworksHW()
                     , multiple = TRUE)
     })
     # Table
@@ -100,88 +164,10 @@ server <- function(input, output) {
             filter(homework_id %in% input$hwPicker) %>%
             select(student_id, first_name, last_name, homework_id, grade)
         
-        datatable(df)
-    })
-    # Review A Data
-    df_review_a <- reactive({
-        req(input$studentPicker, input$topicPicker)
-        
-        review_a <- data$review_a
-        names <- data$student_names
-        merged <- merge(review_a, names, by = "ID") %>%
-            mutate(ID = as.character(ID))
-        
-        filtered <- merged  %>%
-            filter(ID %in% input$studentPicker) %>%
-            filter(topic %in% input$topicPicker)
-    })
-    
-    # DT output
-    output$review_a_dt <- renderDT({
-        df <- df_review_a()
-        df <- df %>%
-            select(ID, First, Last, Topic = topic, Grade = grade)
-        
         datatable(df, rownames = FALSE)
     })
     
-    # Value boxes 
-    # Mastery
-    total_m <- reactive({
-        df <- df_review_a()
-        grades <- df$grade
-        table <- table(grades)
-        total <- table[names(table)=="M"]
-    })
-    output$total_mastered <- renderValueBox(
-        valueBox(total_m(), subtitle = "Total Masteries")
-    )
     
-    # Journeymen
-    total_j <- reactive({
-        df <- df_review_a()
-        grades <- df$grade
-        table <- table(grades)
-        total <- table[names(table)=="J"]
-    })
-    output$total_journey <- renderValueBox(
-        valueBox(total_j(), subtitle = "Total Journeymen")
-    )
-    
-    # Apprentice
-    total_a <- reactive({
-        df <- df_review_a()
-        grades <- df$grade
-        table <- table(grades)
-        total <- table[names(table)=="A"]
-    })
-    output$total_apprentice <- renderValueBox(
-        valueBox(total_a(), subtitle = "Total Apprentice")
-    )
-    # Graphs ----
-    # Fake Data
-    df <- data.frame(
-        x = seq(50),
-        y = rnorm(50, 10, 3),
-        z = rnorm(50, 11, 2),
-        w = rnorm(50, 9, 2)
-    )
-    # Line Chart 
-    output$sample_line <- renderEcharts4r({
-        df %>% 
-            e_charts(x) %>% 
-            e_line(z) %>% 
-            e_area(w) %>% 
-            e_title("Line and area charts")
-    })
-    
-    output$sample_bar <- renderEcharts4r({
-        df %>% 
-            e_charts(x) %>% 
-            e_bar(y, name = "Serie 1") %>% 
-            e_step(z, name = "Serie 2") %>% 
-            e_title("Bar and step charts")
-    })
 }
 
 # Run the application 
