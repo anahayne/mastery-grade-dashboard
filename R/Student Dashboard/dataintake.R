fa658ca96dee78c19bc1a80753f5bbc5bcb688e1
Grace Petty <gracepetty@Graces-MacBook-Pro.local>
2020-04-30 13:21:07 -0400

    More comments-- plz check

--- R/Student Dashboard/dataintake.R
+++ R/Student Dashboard/dataintake.R
@@ -2,33 +2,42 @@
 con  <- db_connect()
 
 # Pull each data table from SQL ----
+#Topics
 sql_query <- 'Select * from Shiny.dbo.topics' 
 df_topics <- dbGetQuery(con, sql_query)
 
+#Reviews
 sql_query <- 'Select * from Shiny.dbo.reviews' 
 df_reviews <- dbGetQuery(con, sql_query)
 
+#Homeworks
 sql_query <- 'Select * from Shiny.dbo.homeworks' 
 df_homeworks <- dbGetQuery(con, sql_query)
 
+#Students
 sql_query <- 'Select * from Shiny.dbo.students' 
 df_students <- dbGetQuery(con, sql_query)
 
+#Review Grades
 sql_query <- 'Select * from Shiny.dbo.reviewGrades' 
 df_reviewGrades <- dbGetQuery(con, sql_query)
 
+#Homework Grades
 sql_query <- 'Select * from Shiny.dbo.homeworkGrades' 
 df_homeworkGrades <- dbGetQuery(con, sql_query)
 
+# Allows values to change and be updated
 reactive <- reactiveValues(df_reviewGrades = df_reviewGrades, df_homeworkGrades = df_homeworkGrades)
 
-# Get Homework Grades
+
+# Database Methods
+# Get Homework Grades-- pulls grades for each student and aggregates all assignments
 getHomeworkGrades <- reactive({
   merge(reactive$df_homeworkGrades, df_students) %>% merge(df_homeworks) %>%
     mutate(firstLast = paste(first_name, last_name))
 }) 
 
-# Get Review Grades
+# Get Review Grades -- pulls reviews for each student and aggregates all assignments
 getReviewGrades <- reactive({
   merge(reactive$df_reviewGrades, df_students) %>% merge(df_reviews) %>%
     mutate(firstLast = paste(first_name, last_name))
--- R/Student Dashboard/libraries.R
+++ R/Student Dashboard/libraries.R
@@ -1,12 +1,16 @@
 # Libraries 
+# library needet to build R studio app
 library(rstudioapi)
+
 # Shiny
+# Shiny libraries used to implement widgets and dashboard layout
 library(shiny)
 library(shinydashboard)
 library(shinydashboardPlus)
 library(shinyWidgets)
 
 # Data
+# libraries used to manipulate data
 library(DT)
 library(dplyr)
 library(lubridate)
@@ -15,8 +19,10 @@ library(tidyverse)
 library(readxl)
 
 # Viz
+# libaraies used in app visualization
 library(echarts4r)
 
 # SQL Drivers
+# used to access and update the database
 library(odbc)
 library(DBI)
